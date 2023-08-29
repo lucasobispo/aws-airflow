330b0b24101286f2dc733466e1646b7dc1c6cdee
Lucas Bispo <lucas.bispo@creditas.com>
2023-08-29 00:12:03 -0300

    add dag

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,3 @@
-FROM apache/airflow:2.1.3-python3.8
-
-ENV AIRFLOW_HOME=/usr/local/airflow
-
-USER root
-
-#configs
-COPY config/entrypoint.sh /entrypoint.sh
-RUN chmod +x /entrypoint.sh
-#COPY config/airflow.cfg ${AIRFLOW_HOME}/airflow.cfg
-
-#plugins
-COPY plugins ${AIRFLOW_HOME}/plugins
-
-#initial dags
-COPY dags /dags
-RUN mkdir ${AIRFLOW_HOME}/dags 
-
-RUN chown -R airflow:airflow ${AIRFLOW_HOME}
-RUN chmod 777 -R /dags
-
-USER airflow
-
-#requirements
-COPY config/requirements.txt .
-RUN pip install --user --no-cache-dir -r requirements.txt
-
-
-EXPOSE 8080 5555 8793
-
-WORKDIR ${AIRFLOW_HOME}
-ENTRYPOINT ["/entrypoint.sh"]
+FROM apache/airflow:2.0.0
+RUN pip install apache-airflow-providers-amazon \
+  && pip install awscli
---
+++ dags/DagEMR/DagEMR.py
@@ -0,0 +1,44 @@
+from airflow.models import DAG
+from airflow.utils.dates import days_ago
+from airflow.hooks.S3_hook import S3Hook
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python import PythonOperator
+
+from plugins.hook.EmrHook import create_job_flow
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(0),
+    'depends_on_past': False
+}
+
+folders = ['dags', 'planning', 'quality']
+
+
+def read_s3_data():
+    s3_hook = S3Hook(aws_conn_id='aws_lab')
+    bucket_name = '638059466675-source'
+    file_name = 'Empregados/glassdoor_consolidado_join_match_less_v2.csv'
+    data = s3_hook.read_key(key=file_name, bucket_name=bucket_name)
+    print(data)
+
+
+with DAG('s3_read_example_with_hook',
+         default_args=args,
+         schedule_interval=None
+         ) as dag:
+    # Only display - start_dag
+    start_dag = DummyOperator(task_id="start_dag")
+
+    read_s3_task = PythonOperator(
+        task_id='read_s3_data',
+        python_callable=read_s3_data,
+    )
+    job_flow_emr = []
+    # Create the EMR cluster
+    create_emr_cluster = create_job_flow(
+        task_id="create_emr_cluster",
+        job_flow_overrides=job_flow_emr,
+        aws_conn_id="aws_default",
+        emr_conn_id="emr_default",
+    )
